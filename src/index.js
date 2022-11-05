910ad3c9046e69f83581138e3c177800d6a2ca40
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-11-05 16:18:55 -0400

    server running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const { conection } = require("../database/conection");
+const express = require("express");
+const cors = require("cors");
 
 conection();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("server on port ", 3000);
+});
