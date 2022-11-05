ab35cc99331bd8671a7412b3bb192dde7b89b4e6
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-11-05 16:24:11 -0400

    first route

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  return res.json({ msg: "welcome!" });
+});
+
 app.listen(3000, () => {
   console.log("server on port ", 3000);
 });
